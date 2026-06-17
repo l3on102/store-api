413b1f785dc42b8125696439a06674ef48faa6b1
leonid <kjhg@>
2026-06-17 17:47:42 +0300

    002 добавление сваггера

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapGet("/weatherforecast", () => "hello store api");
