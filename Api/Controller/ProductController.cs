398c67bb074e73f4dd7a388aaf84719eb8b2cc7a
leonid <kjhg@>
2026-06-26 00:34:05 +0300

    008 дбавлен get в ProductController

--- Api/Controller/ProductController.cs
+++ Api/Controller/ProductController.cs
@@ -1,11 +1,23 @@
+using Api.Data;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controller
 {
-    [ApiController]
-    [Route("api/[controller]")]
     public class ProductController : StoreController
     {
 
+        public ProductController(AppDbContext dbContext)
+        : base(dbContext)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await dbContext.Products.ToListAsync());
+        }
     }
 }
--- Api/Controller/StoreController.cs
+++ Api/Controller/StoreController.cs
@@ -1,3 +1,4 @@
+using Api.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controller
@@ -6,6 +7,11 @@ namespace Api.Controller
     [Route("api/[controller]/[action]")]
     public class StoreController : ControllerBase
     {
+        protected readonly AppDbContext dbContext;
 
+        public StoreController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
     }
 }
