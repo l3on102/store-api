2c763d2adde3f6a93c6e9845f0a640ad9416613b
leonid <kjhg@>
2026-06-27 00:21:10 +0300

    009 добавлен response server в model

--- Api/Controller/ProductController.cs
+++ Api/Controller/ProductController.cs
@@ -1,5 +1,6 @@
+using System.Net;
 using Api.Data;
-
+using Api.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,13 @@ namespace Api.Controller
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
-            return Ok(await dbContext.Products.ToListAsync());
+            ResponseServer response = new ResponseServer
+            {
+                StatusCode = HttpStatusCode.OK,
+                Result = await dbContext.Products.ToListAsync()
+            };
+
+            return Ok(response);
         }
     }
 }
